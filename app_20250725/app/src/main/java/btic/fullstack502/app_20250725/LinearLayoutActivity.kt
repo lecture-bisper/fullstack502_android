aa5042a7484ca968154d1697f9d5de714bd95bf8
lecture-bisper <bisper@outlook.kr>
2025-07-25 17:59:50 +0900

    android - View컴포넌트 (LinearLayout 사용하기, Gravity, Layout Gravity)

---
+++ app_20250725/app/src/main/java/btic/fullstack502/app_20250725/LinearLayoutActivity.kt
@@ -0,0 +1,24 @@
+package btic.fullstack502.app_20250725
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import btic.fullstack502.app_20250725.databinding.ActivityLinearLayoutBinding
+
+class LinearLayoutActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+
+    val binding = ActivityLinearLayoutBinding.inflate(layoutInflater)
+
+    setContentView(binding.root)
+    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+  }
+}
