65bc3fde1161c971f581ede22a648d143b54f160
lecture-bisper <bisper@outlook.kr>
2025-07-28 17:10:16 +0900

    android - View컴포넌트 (휴대폰 전화 앱 UI 만들기)

---
+++ app_20250728/app/src/main/java/btic/fullstack502/app_20250728/PhoneActivity.kt
@@ -0,0 +1,38 @@
+package btic.fullstack502.app_20250728
+
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import btic.fullstack502.app_20250728.databinding.ActivityPhoneBinding
+import kotlin.time.TimeSource
+
+class PhoneActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+
+    val binding = ActivityPhoneBinding.inflate(layoutInflater)
+
+    setContentView(binding.root)
+    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
